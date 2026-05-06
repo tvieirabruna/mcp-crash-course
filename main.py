046c3d81aa01d897dac279bb7faaa1c7a1246032
Bruna <brunavieira@nano.ufrj.br>
2026-05-06 16:38:30 -0300

    feat: enhance main.py to initialize session and invoke agent with a math query using HumanMessage

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import asyncio
 
 from dotenv import load_dotenv
 from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
 from mcp import ClientSession, StdioServerParameters
@@ -14,7 +15,7 @@ from mcp.client.stdio import stdio_client
 load_dotenv()
 
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(temperature=0)
 
 stdio_server_params = StdioServerParameters(
     command="python",
@@ -22,7 +23,17 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized.")
+            
+            tools = await load_mcp_tools(session)
+            
+            agent = create_agent(llm, tools)
+            
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":    
